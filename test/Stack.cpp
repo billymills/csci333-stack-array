79b31079672712fe1992718cd96432f50aa08bef
billy <billy@debian.(none)>
2012-09-17 19:37:32 -0400

    added another test

--- test/Stack.cpp
+++ test/Stack.cpp
@@ -2,11 +2,12 @@
 #include "../src/Stack.h"
 
 TEST(StackTest, Push) {
-  Stack* s = new Stack();
-  s->push(6);
+  	Stack* s = new Stack();
+  	s->push(6);
 
-  EXPECT_EQ(6, s->peek());
-  delete s;
+ 	EXPECT_EQ(6, s->peek());
+  	EXPECT_EQ(1, s->size()); 
+	delete s;
 }
 
 TEST(StackTest, Resize) {
@@ -15,7 +16,26 @@ TEST(StackTest, Resize) {
 		s->push(i);
 	}
 	EXPECT_EQ(11, s->size());
-	delete s;
+	delete s;	
 }
+
+TEST(StackTest, ResizeAgain) {
+	Stack* t = new Stack();	
+	for(int j=1;j<=21;j++){
+		t->push(j);
+	}
+	EXPECT_EQ(21, t->size());
+	delete t;	
+}
+
+/*
+TEST(StackTest, PushPastCapacity) {
+	Stack* s = new Stack();
+	for(int i = 0; i<999; i++) {
+		s->push(i);
+	}
+	EXPECT_EQ(999, s->peek());
+
+}*/
 
 
